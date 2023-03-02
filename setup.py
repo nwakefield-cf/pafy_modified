e88c472ab5d5b793dd7a8bd7c07124b4f7eec092
Nathan Wakefield <nathan@comfreight.com>
2023-03-01 16:48:15 -0800

    Fix rename

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ from setuptools import setup
 from pafy import __version__
 
 setup(
-    name='pafy-modified',
-    packages=['pafy-modified'],
+    name='pafy',
+    packages=['pafy'],
     scripts=['scripts/ytdl'],
     version=__version__,
     description="Retrieve YouTube content and metadata",
